e4654e749d9160853b3620ce60c1a4d49aedbfa6
JeongSu Park <bechalrang@gmail.com>
2022-03-16 13:55:39 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for (int i = 1; i <= n; i += 1) {
+			for (int j = n - 1; j >= i; j -= 1) {
+				sb.append(' ');
+			}
+			for (int j = 1; j <= i; j += 1) {				
+				sb.append("*");
+			}
+			sb.append('\n');
+		}
+		
+		System.out.print(sb.toString());
+	}
+}
