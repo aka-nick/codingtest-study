c39441b0eaa24159d47f60bb07fba4784fbf8ad8
JeongSu Park <bechalrang@gmail.com>
2022-03-18 20:05:36 +0900

    Time: 1 ms (100.00%), Space: 42.1 MB (75.77%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -6,6 +6,11 @@ class Solution {
 		for (int right = 0; right < nums.length; right += 1) {
 			
 			sum += nums[right];
+            
+            if (length > -1 && right - left + 1 > length) {
+				sum -= nums[left];
+				left += 1;
+			}
 			
 			while (sum >= target) {
 				length = length == -1 ? right - left + 1 : Math.min(right - left + 1, length);
