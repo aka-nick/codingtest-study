aeb811f9af4a20bfa2f5a3ab48511993355436a4
JeongSu Park <bechalrang@gmail.com>
2022-03-19 11:17:30 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (65.87%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -3,26 +3,24 @@ class Solution {
 		int left = 0, right = nums.length - 1, i = 0;
 
 		while (i <= right) {
-			int tmp = 0;
-			
 			if (nums[i] == 0) {
-				tmp = nums[i];
-				nums[i] = nums[left];
-				nums[left] = tmp;
-				
+				arrswap(nums, i, left);
 				left += 1;
 				i += 1;
 			}
 			else if (nums[i] == 2) {
-				tmp = nums[i];
-				nums[i] = nums[right];
-				nums[right] = tmp;
-				
+				arrswap(nums, i, right);
 				right -= 1;
 			}
 			else { // nums[i] == 1
 				i += 1;
 			}
 		}
+	}
+	
+	public void arrswap(int[] nums, int s1, int s2) {
+		int tmp = nums[s1];
+		nums[s1] = nums[s2];
+		nums[s2] = tmp;
 	}
 }
