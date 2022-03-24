fd7996324c682f6a5b6a3d2eab802a9df2bfe7bf
JeongSu Park <bechalrang@gmail.com>
2022-03-24 11:04:37 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (87.41%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,11 +2,9 @@ class Solution {
     public void rotate(int[][] matrix) {
 		
 		int n = matrix.length;
-		int swapI = n / 2;
-		int swapJ = swapI + n % 2;
 		
-		for (int i = 0; i < swapI; i += 1) {
-			for (int j = 0; j < swapJ; j += 1) {
+		for (int i = 0; i < (n / 2); i += 1) {
+			for (int j = 0; j < (n / 2 + n % 2); j += 1) {
 				int tmp = matrix[i][j];
 				matrix[i][j] = matrix[n - j - 1][i];
 				matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
