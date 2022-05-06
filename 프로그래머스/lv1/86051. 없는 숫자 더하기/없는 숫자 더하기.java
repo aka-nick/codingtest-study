2669925bc39b6af70ab33dee43706989663b7843
JeongSu Park <bechalrang@gmail.com>
2022-05-06 20:57:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] numbers) {        
-        ArrayList<Integer> zeroToNine = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
-        ArrayList<Integer> numList = new ArrayList<Integer>();
-        for (int num : numbers) numList.add(num);
+        int result = 45; // sum 1~9
         
-        zeroToNine.removeAll(numList);
+        for (int number : numbers) result -= number;
         
-        int answer = 0;
-        for (int value : zeroToNine) answer += value;
-        
-        return answer;
-    }
+        return result;
+    }   
 }
