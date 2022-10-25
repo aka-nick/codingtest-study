b7d3fbd3ecd4f22ce2092c715c648cb2c7ccfe4a
JeongSu Park <bechalrang@gmail.com>
2022-10-25 10:23:51 +0900

    Update 캠핑.java
    리팩토링 연습을 해보았음.

--- 백준/Bronze/4796. 캠핑/캠핑.java
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -4,34 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int days = 1;
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int l = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            if (l == 0 && p == 0 && v == 0) break;
-
-            int total = 0;
-
-            total += (v / p) * l;
-            if (v % p < l) {
-                total += v % p;
-            }
-            else {
-                total += l;
-            }
-
-            sb.append(String.format("Case %d: %d", days++, total)).append(System.lineSeparator());
-        }
-        br.close();
-
-        System.out.println(sb.toString());
+        calculateCampingDays();
     }
+
+    private static void calculateCampingDays() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+
+        int numberOfCase = 1;
+        while (true) {
+            String readLine = br.readLine();
+
+            if (isItOver(new StringTokenizer(readLine))) break;
+
+            result.append(String.format("Case %d: %d", numberOfCase++, getTotalDays(new StringTokenizer(readLine))))
+                    .append(System.lineSeparator());
+        }
+
+        br.close();
+        System.out.println(result);
+    }
+
+    private static boolean isItOver(StringTokenizer st) {
+
+        boolean result = false;
+
+        int l = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        if (l == 0 && p == 0 && v == 0) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private static int getTotalDays(StringTokenizer st) {
+
+        int result = 0;
+
+        int l = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        result += (v / p) * l;
+        if (v % p < l) {
+            result += v % p;
+        }
+        else {
+            result += l;
+        }
+
+        return result;
+    }
+
 }
