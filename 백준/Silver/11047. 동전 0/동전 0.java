1f905521dd924ba57f7b09970648ac7a8d014fbf
JeongSu Park <bechalrang@gmail.com>
2022-10-25 10:59:42 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.valueOf(br.readLine()));
+        }
+        br.close();
+
+        int result = 0;
+        for (int i = list.size() - 1; 0 <= i; i--) {
+            Integer nowToken = list.get(i);
+            while (nowToken <= k) {
+                k -= nowToken;
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+}
