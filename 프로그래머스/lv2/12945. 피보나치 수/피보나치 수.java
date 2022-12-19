e08d9da4cbd45b31d38fdf20ca868a5e4fb6eb81
JeongSu Park <bechalrang@gmail.com>
2022-12-19 12:55:59 +0900

    [level 2] Title: 피보나치 수, Time: 2.51 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        long a = 1L;
+        long b = 1L;
+        for (long i = 2; i < n; i++) {
+            long fibo = a + b;
+            a = b;
+            b = fibo % 1234567;
+        }
+        return Long.valueOf(b % 1234567).intValue();
+    }
+}
