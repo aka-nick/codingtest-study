eca5bb845e8bdfc127462d8c81b093c0f5e46331
JeongSu Park <bechalrang@gmail.com>
2022-12-24 09:32:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,24 +2,13 @@ class Solution {
 
     public int solution(int n) {
 
-        int numberOfOne = getNumberOfOne(n);
         for (int i = n + 1; i <= 1000000; i++) {
-            if (numberOfOne == getNumberOfOne(i)) {
+            if (Integer.bitCount(n) == Integer.bitCount(i)) {
                 return i;
             }
         }
 
         return n;
-    }
-
-    private int getNumberOfOne(int n) {
-        int count = 0;
-        for (char c : Integer.toString(n, 2).toCharArray()) {
-            if (c == '1') {
-                count++;
-            }
-        }
-        return count;
     }
 
 }
