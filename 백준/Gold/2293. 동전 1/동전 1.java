f08c0ac6945d72f28005990ab4806ba7a207c483
JeongSu Park <bechalrang@gmail.com>
2023-01-27 15:06:03 +0900

    [Gold V] Title: 동전 1, Time: 144 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,31 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int coinSize = sc.nextInt();
-        int amount = sc.nextInt();
-        sc.nextLine();
-        int[] coins = new int[coinSize];
-        for (int i = 0; i < coins.length; i++) {
-            coins[i] = Integer.valueOf(sc.nextLine());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
-        sc.close();
+        br.close();
 
-        long[] bottomup = new long[amount + 1];
-        bottomup[0] = 1L;
+        int[][] cases = new int[n + 1][k + 1];
+        Arrays.stream(cases).forEach(row -> row[0] = 1);
 
-        for (int i = 0; i < coins.length ; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j >= coins[i]) {
-                    bottomup[j] += bottomup[j - coins[i]];
+        for (int coinIdx = 1; coinIdx < n + 1; coinIdx++) {
+            for (int price = 1; price < k + 1; price++) {
+
+                if (0 <= price - coins[coinIdx]) {
+                    int prePriceCaseCount = cases[coinIdx][price - coins[coinIdx]];
+                    int preCoinCaseCount = cases[coinIdx - 1][price];
+                    cases[coinIdx][price] = prePriceCaseCount + preCoinCaseCount;
+                }
+                else {
+                    cases[coinIdx][price] = cases[coinIdx - 1][price];
                 }
             }
         }
 
-        System.out.println(bottomup[amount]);
+        System.out.println(cases[n][k]);
     }
-    
+
 }
