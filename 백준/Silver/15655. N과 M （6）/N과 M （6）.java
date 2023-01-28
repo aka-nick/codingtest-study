682101cb0e601cc5d9797c4c626ad64cd11c2d62
JeongSu Park <bechalrang@gmail.com>
2023-01-28 10:58:18 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n;
+    private static int m;
+    private static int[] selected;
+    private static StringBuilder result;
+    private static List<Integer> numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            Integer num = Integer.valueOf(st.nextToken());
+            numbers.add(num);
+        }
+        br.close();
+
+        numbers.sort((o1, o2) -> o1 - o2);
+        selected = new int[m];
+        result = new StringBuilder();
+
+        bt(0);
+
+        System.out.println(result);
+    }
+
+    private static void bt(int depth) {
+        if (depth == m) {
+            for (int i : selected) {
+                result.append(i).append(' ');
+            }
+            result.append(System.lineSeparator());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int number = numbers.get(i);
+            if (0 < depth && number <= selected[depth - 1]) {
+                continue;
+            }
+
+            selected[depth] = number;
+            bt(depth + 1);
+        }
+    }
+}
