fd002d4aa3a3ace1f95f8910a954043f1b2b72a2
JeongSu Park <bechalrang@gmail.com>
2023-01-29 15:34:48 +0900

    [Silver II] Title: 최대 힙, Time: 364 ms, Memory: 27968 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    private static StringBuilder result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+        br.close();
+
+        result = new StringBuilder();
+        queueing(numbers);
+
+        System.out.println(result);
+    }
+
+    private static void queueing(List<Integer> numbers) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (Integer number : numbers) {
+            if (number == 0) {
+                int addValue = pq.isEmpty() ?
+                        0 : pq.poll();
+                result.append(addValue).append(System.lineSeparator());
+                continue;
+            }
+
+            pq.add(number);
+        }
+    }
+
+}
