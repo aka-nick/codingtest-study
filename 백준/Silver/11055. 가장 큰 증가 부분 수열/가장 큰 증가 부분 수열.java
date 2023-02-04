3b64aef257f4b5cbc5faa7c048d7d7a300ba1443
JeongSu Park <bechalrang@gmail.com>
2023-02-04 09:42:27 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 148 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        int[] mem = new int[n];
+        mem[0] = a[0];
+        for (int i = 1; i < n; i++) {
+            int idx = i;
+            int maxIdx = i;
+            while (idx-- > 0) {
+                if (a[idx] < a[i]) {
+                    if (mem[maxIdx] < mem[idx]) {
+                        maxIdx = idx;
+                    }
+                }
+            }
+
+            mem[i] = a[i];
+            if (maxIdx != i) {
+                mem[i] = mem[maxIdx] + a[i];
+            }
+        }
+
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(mem[i], max);
+        }
+
+        System.out.println(max);
+    }
+
+}
