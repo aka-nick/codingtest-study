7f50d8d9fea45a0289dccdc494843fa42be85fcd
JeongSu Park <bechalrang@gmail.com>
2023-02-09 18:31:22 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 44844 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    private static StringBuilder result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            set.add(Integer.valueOf(st.nextToken()));
+            a[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(a);
         int m = Integer.parseInt(br.readLine());
+        int[] f = new int[m];
         st = new StringTokenizer(br.readLine());
-        List<Integer> finds = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            finds.add(Integer.valueOf(st.nextToken()));
+            f[i] = Integer.parseInt(st.nextToken());
         }
         br.close();
 
-        StringBuilder sb = new StringBuilder();
-        for (Integer find : finds) {
-            if (set.contains(find)) {
-                sb.append(1);
+        result = new StringBuilder();
+        for (int target : f) {
+            int l = 0;
+            int r = n - 1;
+
+            while (l <= r) {
+                int mid = (l + r) / 2;
+
+                if (a[mid] == target) {
+                    printAvailable();
+                    break;
+                }
+                else if (a[mid] < target) {
+                    l = mid + 1;
+                }
+                else {
+                    r = mid - 1;
+                }
+
+
             }
-            else {
-                sb.append(0);
+
+            if (r < l) {
+                printUnavailable();
             }
-            sb.append(System.lineSeparator());
         }
 
-        System.out.println(sb);
+        System.out.println(result);
     }
+
+    private static void printUnavailable() {
+        result.append(0).append(System.lineSeparator());
+    }
+
+    private static void printAvailable() {
+        result.append(1).append(System.lineSeparator());
+    }
+
 }
