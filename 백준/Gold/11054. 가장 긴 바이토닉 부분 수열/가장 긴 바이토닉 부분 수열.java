e840272b2e19496ac4ef4bf6a847e0df90be229b
JeongSu Park <bechalrang@gmail.com>
2023-03-03 12:13:44 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 160 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,51 +8,57 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> numbers = new ArrayList<>();
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numbers.add(Integer.valueOf(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         br.close();
 
-        int[] mem1 = new int[n];
-        mem1[0] = 1;
+        int[] ascArr = new int[n];
+        ascArr[0] = 1;
         for (int i = 0; i < n; i++) {
+            int now = arr[i];
             int idx = i;
-            int maxIdx = i;
+            int largestValueForUpdate = -1;
             while (idx-- > 0) {
-                if (numbers.get(idx) < numbers.get(i) &&
-                        mem1[maxIdx] < mem1[idx]) {
-                    maxIdx = idx;
-                    mem1[i] = mem1[maxIdx] + 1;
+                int pre = arr[idx];
+                if (pre < now) {
+                    if (largestValueForUpdate < ascArr[idx] + 1) {
+                        largestValueForUpdate = ascArr[idx] + 1;
+                    }
                 }
             }
-            if (mem1[i] == 0) {
-                mem1[i] = 1;
-            }
+
+            ascArr[i] = largestValueForUpdate == -1 ? 1 : largestValueForUpdate;
         }
 
-        int max = -1;
-        int[] mem2 = new int[n];
-        mem2[n - 1] = 1;
+        int[] descArr = new int[n];
+        descArr[n - 1] = 1;
         for (int i = n - 1; 0 <= i; i--) {
+            int now = arr[i];
             int idx = i;
-            int maxIdx = i;
+            int largestValueForUpdate = -1;
             while (++idx < n) {
-                if (numbers.get(idx) < numbers.get(i) &&
-                    mem2[maxIdx] < mem2[idx]) {
-                    maxIdx = idx;
-                    mem2[i] = mem2[maxIdx] + 1;
+                int pre = arr[idx];
+                if (pre < now) {
+                    if (largestValueForUpdate < descArr[idx] + 1) {
+                        largestValueForUpdate = descArr[idx] + 1;
+                    }
                 }
             }
-            if (mem2[i] == 0) {
-                mem2[i] = 1;
-            }
 
-            max = Math.max(mem1[i] + mem2[i], max);
+            descArr[i] = largestValueForUpdate == -1 ? 1 : largestValueForUpdate;
         }
 
-        System.out.println(max - 1);
+        int answer = -1;
+        for (int i = 0; i < n; i++) {
+            if (answer < ascArr[i] + descArr[i]) {
+                answer = ascArr[i] + descArr[i];
+            }
+        }
+
+        System.out.println(answer - 1);
     }
 
 }
