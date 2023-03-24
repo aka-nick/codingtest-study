416494ec25a8aeb11f6344b76992ec4c7e415518
JeongSu Park <bechalrang@gmail.com>
2023-03-24 12:03:19 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 5.73 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int rows, int columns, int[][] queries) {
+
+        int[][] matrix = new int[rows + 1][columns + 1]; // one-based array
+        int maxrixValue = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                matrix[i][j] = maxrixValue++;
+            }
+        }
+
+//            testPrint(matrix);
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            // 시작좌표, 끝좌표 획득
+            int[] query = queries[i];
+            int startX = query[0];
+            int startY = query[1];
+            int endX = query[2];
+            int endY = query[3];
+
+            result[i] = rotate(matrix, startX, startY, endX, endY);
+        }
+
+        return result;
+    }
+
+    private int rotate(int[][] matrix, int startX, int startY, int endX, int endY) {
+        // 회전 로직 구현부
+        int tmp1 = 0; // 뽑은거
+        int tmp2 = 0; // 뽑을거 (뽑은걸 꽂을 곳의 숫자)
+        // 먼저 맨윗줄을 옮긴다고 해보자 : startX, startY =(y + 1)=> startX, endY
+        int x = startX;
+        int y = startY;
+        int min = matrix[x][y];
+        for (; y < endY; y++) {
+            // 최초 뽑기
+            if (tmp1 == 0) tmp1 = matrix[x][y];
+
+            // 꽂기
+            tmp2 = matrix[x][y + 1];
+            matrix[x][y + 1] = tmp1;
+
+            // 뽑기
+            tmp1 = tmp2;
+
+            // 최소 체크
+            min = Math.min(min, tmp1);
+        }
+
+        // 그다음은 오른쪽 위에서 아래로 : startX, endY =(x + 1)=> endX, endY
+        x = startX;
+        y = endY;
+        tmp1 = tmp2;
+        for (; x < endX; x++) {
+            // 꽂기
+            tmp2 = matrix[x + 1][y];
+            matrix[x + 1][y] = tmp1;
+
+            // 뽑기
+            tmp1 = tmp2;
+
+            // 최소 체크
+            min = Math.min(min, tmp1);
+        }
+
+        // 그담은 오른쪽 아래에서 왼쪽 아래로 : endX, endY =(y - 1)=> endX, startY
+        x = endX;
+        y = endY;
+        tmp1 = tmp2;
+
+        for (; startY < y; y--) {
+            // 꽂기
+            tmp2 = matrix[x][y - 1];
+            matrix[x][y - 1] = tmp1;
+//                    System.out.println(tmp2 + " : " + tmp1);
+
+            // 뽑기
+            tmp1 = tmp2;
+
+            // 최소 체크
+            min = Math.min(min, tmp1);
+        }
+//                System.out.println("tmp1 = " + tmp1);
+//                System.out.println("tmp2 = " + tmp2);
+
+        // 마지막은 왼쪽 아래에서 왼쪽 위로 : endX, startY =(x - 1)=> startX, startY
+        x = endX;
+        y = startY;
+        tmp1 = tmp2;
+
+        for (; startX < x; x--) {
+            // 꽂기
+            tmp2 = matrix[x - 1][y];
+            matrix[x - 1][y] = tmp1;
+//                    System.out.println(tmp2 + " : " + tmp1);
+
+            // 뽑기
+            tmp1 = tmp2;
+
+            // 최소 체크
+            min = Math.min(min, tmp1);
+        }
+
+        return min;
+    }
+
+    private void testPrint(int[][] matrix) {
+        System.out.println("++++++++++");
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print("\t" + anInt + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("++++++++++");
+    }
+}
