428073d04e302672c067e25da283239050c83fc9
JeongSu Park <bechalrang@gmail.com>
2023-12-07 21:44:31 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 128 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
-		
-		for (int i = n; i >= 1; i -= 1) {
-			for (int j = 1; j <= i; j += 1) {				
-				sb.append("*");
-			}
-			sb.append('\n');
-		}
-		
-		System.out.print(sb.toString());
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        String star = "*";
+        while (n > 0) {
+            System.out.println(star.repeat(n--));
+        }
+    }
+
 }
