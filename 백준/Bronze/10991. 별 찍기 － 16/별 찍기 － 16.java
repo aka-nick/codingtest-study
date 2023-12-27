bc801a1ba9e06797139f50aba68d23c6a43a972b
JeongSu Park <bechalrang@gmail.com>
2023-12-27 15:19:53 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
-		
-		for (int i = 1; i <= n; i += 1) {
-			
-			for (int j = n - 1; j >= i; j -= 1) sb.append(' ');
-			
-			for (int j = 1; j <= i * 2 - 1; j += 1) {
-				char tmp = j % 2 != 0 ? '*' : ' ';
-				sb.append(tmp);				
-			}
-			sb.append('\n');
-		}
-		
-		System.out.print(sb.toString());
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            int term = n - i;
+            result.append(" ".repeat(term));
+            for (int j = 0; j < i; j++) {
+                result.append("*").append(" ");
+            }
+            result.append(System.lineSeparator());
+        }
+
+        System.out.println(result);
+    }
+
 }
