dc1ea4e016226c0b92453a148ef8bb97aff27cf5
JeongSu Park <bechalrang@gmail.com>
2023-12-28 10:54:44 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -1,23 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		StringBuffer sb = new StringBuffer();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
-		
-		for (int i = 1; i <= n; i += 1) {
-			
-			for (int j = n - 1; j >= i; j -= 1) sb.append(' ');
-			
-			for (int j = 1; j <= i * 2 - 1; j += 1) {
-				char tmp = (i == n || j == 1 || j == i * 2 - 1) ? '*' : ' ';
-				sb.append(tmp);				
-			}
-			sb.append('\n');
-		}
-		
-		System.out.print(sb.toString());
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+
+            if (i == 1) {
+                result.append(" ".repeat(n - i));
+                result.append("*").append(System.lineSeparator());
+                continue;
+            }
+            if (i == n) {
+                result.append("*".repeat(n * 2 - 1)).append(System.lineSeparator());
+                continue;
+            }
+
+            result
+                    .append(" ".repeat(n - i))
+                    .append("*")
+                    .append(" ".repeat((i - 1) * 2 - 1))
+                    .append("*")
+                    .append(System.lineSeparator());
+        }
+
+        System.out.println(result);
+    }
+
 }
