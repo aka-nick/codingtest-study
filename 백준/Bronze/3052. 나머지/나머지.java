b5106e03dd58bb61b0336954c37a89fb0876dd41
JeongSu Park <bechalrang@gmail.com>
2023-12-29 22:10:42 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,25 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer[] mods = new Integer[42];
+        Set<Integer> mods = new HashSet<>();
         for (int i = 0; i < 10; i++) {
-            mods[Integer.parseInt(br.readLine()) % 42] = 1;
+            mods.add(Integer.parseInt(br.readLine()) % 42);
         }
         br.close();
 
-        int diff = 0;
-        for (Integer mod : mods) {
-            if (mod != null) {
-                diff++;
-            }
-        }
-
-        System.out.println(diff);
+        System.out.println(mods.size());
     }
 
 }
