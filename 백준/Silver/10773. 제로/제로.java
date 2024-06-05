9ad9b3947525eeb546266377ebe5d29f60f8a995
JeongSu Park <bechalrang@gmail.com>
2024-06-06 08:17:20 +0900

    [Silver IV] Title: 제로, Time: 256 ms, Memory: 23908 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,44 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> prices = new ArrayList<>();
-        setPrices(br, n, prices);
-        br.close();
-
-        Deque<Integer> resultStack = new ArrayDeque<>();
-        for (Integer price : prices) {
-            if (price == 0) {
-                if (!resultStack.isEmpty()) {
-                    resultStack.pop();
-                }
+        int k = Integer.parseInt(br.readLine());
+        Deque<Integer> d = new ArrayDeque<>();
+        while (k-- > 0) {
+            int now = Integer.parseInt(br.readLine());
+            if (now == 0) {
+                d.pop();
                 continue;
             }
-
-            resultStack.push(price);
+            d.push(now);
         }
+        br.close();
 
-        long result = 0;
-        for (Integer price : resultStack) {
-            result += price;
-        }
+        long sum = d.stream()
+                .mapToLong(Integer::longValue)
+                .sum();
 
-        System.out.println(result);
+        System.out.println(sum);
     }
 
-    private static void setPrices(BufferedReader br, int n, List<Integer> prices)
-            throws IOException {
-        for (int i = 0; i < n; i++) {
-            prices.add(Integer.parseInt(br.readLine()));
-        }
-    }
-    
 }
