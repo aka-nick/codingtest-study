ae358a71ea745c1115ec0d85318425f9c4a33994
JeongSu Park <bechalrang@gmail.com>
2024-06-25 06:56:20 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 17204 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -12,53 +11,57 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         StringBuilder result = new StringBuilder();
         while (t-->0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int numberOfCabbage = Integer.parseInt(st.nextToken());
-            int[][] map = new int[n][m];
-            for (int i = 0; i < numberOfCabbage; i++) {
-                st = new StringTokenizer(br.readLine());
-                map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+            String[] split = br.readLine().split(" ");
+            int m = Integer.parseInt(split[0]);
+            int n = Integer.parseInt(split[1]);
+            int k = Integer.parseInt(split[2]);
+            boolean[][] map = new boolean[m + 1][n + 1];
+            while (k-->0) {
+                String[] sp = br.readLine().split(" ");
+                int x = Integer.parseInt(sp[0]);
+                int y = Integer.parseInt(sp[1]);
+
+                map[x][y] = true;
             }
 
-            boolean[][] visited = new boolean[n][m];
             int[] dx = {1, 0, -1, 0};
             int[] dy = {0, 1, 0, -1};
             Deque<int[]> q = new ArrayDeque<>();
+            int count = 0;
+            for (int x = 0; x < m; x++) {
+                for (int y = 0; y < n; y++) {
+                    if (!map[x][y]) {
+                        continue;
+                    }
 
-            int numberOfWorm = 0;
-            for (int x = 0; x < n; x++) {
-                for (int y = 0; y < m; y++) {
-                    if (!visited[x][y] && map[x][y] == 1) {
-                        q.addLast(new int[]{x, y});
-                        visited[x][y] = true;
-                        numberOfWorm++;
+                    q.addLast(new int[]{x, y});
+                    map[x][y] = false;
+                    count++;
 
-                        while (!q.isEmpty()) {
-                            int[] pos = q.pollFirst();
-                            for (int i = 0; i < 4; i++) {
-                                int nx = pos[0] - dx[i];
-                                int ny = pos[1] - dy[i];
+                    while (!q.isEmpty()) {
+                        int[] now = q.removeFirst();
+                        for (int i = 0; i < 4; i++) {
+                            int nx = now[0] - dx[i];
+                            int ny = now[1] - dy[i];
 
-                                if (nx < 0 || ny < 0 || n <= nx || m <= ny ||
-                                        visited[nx][ny] ||
-                                        map[nx][ny] == 0) {
-                                    continue;
-                                }
-
-                                q.addLast(new int[]{nx, ny});
-                                visited[nx][ny] = true;
+                            if (nx < 0 || ny < 0 || m < nx || n < ny) {
+                                continue;
                             }
+                            if (!map[nx][ny]) {
+                                continue;
+                            }
+
+                            map[nx][ny] = false;
+                            q.addLast(new int[]{nx, ny});
                         }
                     }
                 }
             }
-
-            result.append(numberOfWorm).append(System.lineSeparator());
+            result.append(count).append(System.lineSeparator());
         }
         br.close();
 
         System.out.println(result);
     }
+
 }
