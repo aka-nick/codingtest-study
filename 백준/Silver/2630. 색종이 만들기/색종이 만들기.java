8c33ac8e0e9a1870f8ecba21da5bfdbf3ef9a9d2
JeongSu Park <bechalrang@gmail.com>
2024-06-29 17:40:55 +0900

    [Silver II] Title: 색종이 만들기, Time: 140 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,105 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int WHITE = 0;
-    private static final int BLUE = 1;
-    static int countOfWhite = 0;
-    static int countOfBlue = 0;
-    static int[][] map;
+    private static int[][] map;
+    private static int wCount = 0;
+    private static int bCount = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         map = new int[n][n];
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] numbers = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(numbers[j]);
             }
         }
         br.close();
 
-        countConfetti(n, 0, 0);
+        doOrigami(0, 0, n);
 
-        System.out.println(countOfWhite);
-        System.out.println(countOfBlue);
+        System.out.println(wCount);
+        System.out.println(bCount);
     }
 
-    private static void countConfetti(int n, int x, int y) {
-        if (n == 1) {
-            baseCase(n, x, y);
+    private static void doOrigami(int x, int y, int size) {
+        boolean isAllSame = true;
+        compare:
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (map[x][y] != map[i][j]) {
+                    isAllSame = false;
+                    break compare;
+                }
+            }
+        }
+        if (isAllSame) {
+            if (map[x][y] == 0) {
+                wCount++;
+            } else {
+                bCount++;
+            }
             return;
         }
 
-        int standardColor = map[x][y];
-        boolean isAllSame = isAllSame(n, x, y, standardColor);
-        if (isAllSame) {
-            if (standardColor == WHITE) {
-                countOfWhite++;
-            }
-            else {
-                countOfBlue++;
-            }
-        }
-        else {
-            int nextSize = n / 2;
-            countConfetti(nextSize, x, y);
-            countConfetti(nextSize, x, y + nextSize);
-            countConfetti(nextSize, x + nextSize, y);
-            countConfetti(nextSize, x + nextSize, y + nextSize);
-        }
+        int halfSize = size / 2;
+        doOrigami(x, y, halfSize);
+        doOrigami(x, y + halfSize, halfSize);
+        doOrigami(x + halfSize, y, halfSize);
+        doOrigami(x + halfSize, y + halfSize, halfSize);
     }
 
-    private static boolean isAllSame(int n, int x, int y, int standardColor) {
-        for (int nx = x; nx < x + n; nx++) {
-            for (int ny = y; ny < y + n; ny++) {
-                if (standardColor != map[nx][ny]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private static void baseCase(int n, int x, int y) {
-        int tmpStandardColor = map[x][y];
-        int tmpCountOfWhite = 0;
-        int tmpCountOfBlue = 0;
-        boolean isSame = true;
-        for (int nx = x; nx < x + n; nx++) {
-            for (int ny = y; ny < y + n; ny++) {
-                int nowColor = map[nx][ny];
-                if (tmpStandardColor != nowColor) {
-                    isSame = false;
-                }
-                if (nowColor == WHITE) {
-                    tmpCountOfWhite++;
-                }
-                else if (nowColor == BLUE) {
-                    tmpCountOfBlue++;
-                }
-            }
-        }
-        if (isSame) {
-            if (tmpStandardColor == WHITE) {
-                countOfWhite++;
-            }
-            else if (tmpStandardColor == BLUE) {
-                countOfBlue++;
-            }
-        }
-        else {
-            if (tmpStandardColor == WHITE) {
-                countOfWhite += tmpCountOfWhite;
-            }
-            else if (tmpCountOfBlue == BLUE) {
-                countOfBlue += tmpCountOfBlue;
-            }
-        }
-    }
-    
 }
