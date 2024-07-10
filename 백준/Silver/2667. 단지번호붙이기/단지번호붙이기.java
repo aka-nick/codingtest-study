24fd2771c3f8035739cfa5e9f9ad8b7a7c3e9e1e
JeongSu Park <bechalrang@gmail.com>
2024-07-11 05:18:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -11,61 +11,57 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] map = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        boolean[][] map = new boolean[n][n];
+        for (int x = 0; x < n; x++) {
             char[] chars = br.readLine().toCharArray();
-            for (int j = 0; j < n; j++) {
-                map[i][j] = chars[j] - '0';
+            for (int y = 0; y < n; y++) {
+                map[x][y] = chars[y] == '1';
             }
         }
         br.close();
 
-        StringBuilder result = new StringBuilder();
-        int numberOfArea = 0;
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
-        boolean[][] visited = new boolean[n][n];
         Deque<int[]> q = new ArrayDeque<>();
-        List<Integer> areas = new LinkedList<>();
+
+        int count = 0;
+        List<Integer> homeCounts = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
 
         for (int x = 0; x < n; x++) {
             for (int y = 0; y < n; y++) {
-                int area = 0;
-                if (map[x][y] != 0 && !visited[x][y]) {
-                    map[x][y] = ++numberOfArea;
-                    area++;
-                    q.addLast(new int[]{x, y});
-                    visited[x][y] = true;
+                if (!map[x][y]) continue;
 
-                    while (!q.isEmpty()) {
-                        int[] pos = q.pollFirst();
-                        for (int i = 0; i < 4; i++) {
-                            int nx = pos[0] - dx[i];
-                            int ny = pos[1] - dy[i];
+                int homeCount = 0;
+                q.addLast(new int[]{x, y});
+                map[x][y] = false;
+                count++;
+                homeCount++;
+                while (!q.isEmpty()) {
+                    int[] now = q.removeFirst();
+                    for (int i = 0; i < 4; i++) {
+                        int nx = now[0] - dx[i];
+                        int ny = now[1] - dy[i];
 
-                            if (nx < 0 || ny < 0 || n <= nx || n <= ny) {
-                                continue;
-                            }
-                            if (map[nx][ny] == 0 || visited[nx][ny]) {
-                                continue;
-                            }
+                        if (nx < 0 || ny < 0 || n <= nx || n <= ny) continue;
+                        if (!map[nx][ny]) continue;
 
-                            map[nx][ny] = numberOfArea;
-                            area++;
-                            q.addLast(new int[]{nx, ny});
-                            visited[nx][ny] = true;
-                        }
+                        q.addLast(new int[]{nx, ny});
+                        map[nx][ny] = false;
+                        homeCount++;
                     }
-                    areas.add(area);
                 }
+
+                homeCounts.add(homeCount);
             }
         }
 
-        result.append(numberOfArea).append(System.lineSeparator());
-        areas.stream()
+        result.append(count).append(System.lineSeparator());
+        homeCounts.stream()
                 .sorted()
-                .forEach(i -> result.append(i).append(System.lineSeparator()));
+                .forEach(c -> result.append(c).append(System.lineSeparator()));
 
         System.out.println(result);
     }
+
 }
