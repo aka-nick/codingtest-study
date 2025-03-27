640df7f2d7bd6ba881ce70dcfe575cd942d6c713
JeongSu Park <bechalrang@gmail.com>
2025-03-27 20:47:40 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,31 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[m];
 
-        bt(n, m, 1, 0, arr);
+    private static int n;
+    private static int m;
+    private static int[] selected;
+    private static StringBuilder result;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+        selected = new int[m + 1];
+        result = new StringBuilder();
+        br.close();
+
+        bt(0, 1);
+
+        System.out.println(result);
     }
 
-    private static void bt(int n, int m, int start, int depth, int[] arr) {
-        if (depth == m) {
-            StringBuilder sb = new StringBuilder();
-            for (int i : arr) {
-                sb.append(i).append(" ");
+    private static void bt(int countOfSelected, int from) {
+        if (countOfSelected == m) {
+            for (int i = 0; i < m; i++) {
+                result.append(selected[i]).append(' ');
             }
-            System.out.println(sb.toString());
+            result.append(System.lineSeparator());
             return;
         }
 
-        int nowLength = n - start + m;
-        if (nowLength < m) return;
-        for (int i = start; i <= n; i++) {
-                arr[depth] = i;
-                bt(n, m, i + 1, depth + 1, arr);
+        for (int num = from; num <= n; num++) {
+            selected[countOfSelected] = num;
+            bt(countOfSelected + 1, num + 1);
         }
     }
+
 }
