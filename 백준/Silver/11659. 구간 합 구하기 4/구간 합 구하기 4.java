ae5ba845947bbe633ebab6876db94d986b164c1e
JeongSu Park <bechalrang@gmail.com>
2025-04-03 21:34:39 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 59480 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,52 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[] numbers = new int[n + 2];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int m = Integer.parseInt(line[1]);
+        int[] numbers = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] sums = new int[n + 1];
+        sums[1] = numbers[0];
+        for (int i = 2; i <= n; i++) {
+            sums[i] = sums[i - 1] + numbers[i - 1];
         }
-        for (int i = n - 1; 0 < i; i--) {
-            numbers[i] += numbers[i + 1];
-        }
-
-        List<Range> cases = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            cases.add(new Range(Integer.parseInt(st.nextToken())
-                    , Integer.parseInt(st.nextToken())));
-        }
-
-        br.close();
 
         StringBuilder result = new StringBuilder();
-        for (Range range : cases) {
-            result.append(numbers[range.start] - numbers[range.end + 1])
+        while (m-- > 0) {
+            String[] ijLine = br.readLine().split(" ");
+            int i = Integer.parseInt(ijLine[0]);
+            int j = Integer.parseInt(ijLine[1]);
+            result.append(sums[j] - sums[i - 1])
                     .append(System.lineSeparator());
         }
+        br.close();
+
         System.out.println(result);
-    }
-
-    static class Range {
-        int start;
-        int end;
-
-        public Range(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
     }
 
 }
